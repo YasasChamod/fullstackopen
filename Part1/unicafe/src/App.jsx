83667eb5d7bf3135c81552c66a38fdc18cd7590e
YasasChamod <yasaschamod1234@gmail.com>
2026-03-26 20:35:52 +0530

    Part1 completed

---
+++ Part1/unicafe/src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>give feedback</h1>
+    </div>
+  )
+}
+
+const Content = () => {
+  return (
+    <div>
+      <h2>statistics</h2>
+    </div>
+  )
+}
+
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = (props) => {
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  const all = props.good + props.neutral + props.bad
+  const average = all === 0 ? 0 : (props.good - props.bad) / all
+  const positive = all === 0 ? 0 : (props.good * 100) / all
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average.toFixed(1)} />
+          <StatisticLine text="positive" value={`${positive.toFixed(1)} %`} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+  
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Header />
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Content />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
