b25c4b299756f47b2043b19c33db3c18b79d8695
YasasChamod <yasaschamod1234@gmail.com>
2026-04-05 18:22:00 +0530

    Backend connected and proxy added

---
+++ Part3/index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(express.json())
+
+morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '')
+
+app.use(morgan('tiny :body'))
+
+const persons = [
+  {
+    "id": "1",
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": "2",
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": "3",
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": "4",
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const index = persons.findIndex(p => p.id === id)
+  if (index !== -1) {
+    persons.splice(index, 1)
+    response.status(204).end()
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'name or number is missing' })
+  }
+
+  const nameExists = persons.some(p => p.name === body.name)
+  if (nameExists) {
+    return response.status(400).json({ error: 'name must be unique' })
+  }
+
+  const newPerson = {
+    id: Math.floor(Math.random() * 1000000).toString(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons.push(newPerson)
+  response.json(newPerson)
+})
+
+app.get('/info', (request, response) => {
+  const entryCount = persons.length
+  const now = new Date()
+  response.send(
+    `<p>Phonebook has info for ${entryCount} people</p><p>${now}</p>`
+  )
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
